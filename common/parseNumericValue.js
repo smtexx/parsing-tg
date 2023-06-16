8e82460c1d158108091751a9772dee446edb6aad
Roman Kolmakov <smtexx@hotmail.com>
2023-06-16 10:38:01 +0200

    function is ready

--- common/parseNumericValue.js
+++ common/parseNumericValue.js
@@ -1,20 +1,22 @@
 /**
- * Extracts string values like 4.5K, 567, 54.67 from string and converts it to numbers,
- * If received value contains several blocks of digits, or is not string, returns the text "null"
+ * Функция извлекает часть строки из цифровых символов идущих подряд
+ * типа 4.5K, 1.7k, 567, 54.67. Преобразует их в число и возвращает его.
+ * При этом пробельные символы между цифрами удаляются, сиволы K и k приводят
+ * к умножению числа на 1000. Если полученный аргумент имеет неподходящий тип,
+ * содержит более одного или ни одного блока цифр, возвращается 'empty'.
  *
- * @param {string} str - string value to parse
- * @returns {number | 'empty'}
+ * @param {string} str - строка для извлечения цифрового значения
  */
 
 export function parseNumericValue(str) {
-  const regexp = /\d+(\.\d+)?K?/;
+  const regexp = /\d+(\.\d+)?[Kk]?/;
   const EMPTY = 'empty';
 
   if (typeof str !== 'string') {
     return EMPTY;
   }
 
-  const preparedStr = str.replaceAll(' ', '');
+  const preparedStr = str.replace(/\s+/g, '');
   if (!regexp.test(preparedStr) || /\d/.test(preparedStr.replace(regexp, ''))) {
     return EMPTY;
   }
@@ -22,8 +24,8 @@ export function parseNumericValue(str) {
   let extracted = preparedStr.match(regexp)[0];
 
   let kilo = false;
-  if (/K/.test(extracted)) {
-    extracted = extracted.replace('K', '');
+  if (/[Kk]/.test(extracted)) {
+    extracted = extracted.replace(/[Kk]/, '');
     kilo = true;
   }
 
