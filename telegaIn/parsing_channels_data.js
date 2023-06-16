802352eacd5f22b3e2711dce572cbc4422ea5649
Roman Kolmakov <smtexx@hotmail.com>
2023-06-16 12:43:44 +0200

    delete refactored scripts

--- telegaIn/parsing_channels_data.js
+++
@@ -1,9 +0,0 @@
-import { chromium } from 'playwright';
-import { getDataFromTelegaIn } from './scripts/getDataFromTelegaIn.js';
-
-(async () => {
-  const BROWSER = await chromium.launch();
-  const PAGE = await BROWSER.newPage();
-
-  await BROWSER.close();
-})();
