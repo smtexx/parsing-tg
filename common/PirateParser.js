09210a97c69ab3c93f4fc21f1a1e0df94d250d7e
Roman Kolmakov <smtexx@hotmail.com>
2023-06-18 12:04:45 +0200

    Add features: headless, delay, fuse

--- common/PirateParser.js
+++ common/PirateParser.js
@@ -1,4 +1,4 @@
-import { chromium } from 'playwright';
+import { chromium, firefox, webkit } from 'playwright';
 
 /**
  * Класс парсера данных. Последовательность запуска:
@@ -21,6 +21,9 @@ export default class PirateParser {
   #stackLength;
   #errorHandleFn;
   #errorStack = [];
+  #headless;
+  #delay;
+  #fuse = 0;
 
   /**
    * Создать экземпляр парсера
@@ -32,11 +35,21 @@ export default class PirateParser {
    * @property {(PAGE: PAGE) => Promise<PAGE>} authFn - функция авторизации в ходе парсинга
    * @property {(errors: any[]) => Promise<undefined>} errorHandleFn - функция для обработки ошибок, возникших при парсинге
    * @property {number} streams - количество паралельных потоков парсинга
+   * @property {boolean} headless - запустить ли браузер без графического интерфейса
+   * @property {number} delay - пауза между запросами парсинга
    *
    * @param {Config} config - объект конфигурации создаваемого парсера
    *
    */
-  constructor({ parsingFn, saveDataFn, authFn, errorHandleFn, streams }) {
+  constructor({
+    parsingFn,
+    saveDataFn,
+    authFn,
+    errorHandleFn,
+    streams,
+    headless,
+    delay,
+  }) {
     if (!parsingFn || !saveDataFn) {
       throw new PirateParserError('Unable to create parser without argument');
     }
@@ -46,6 +59,8 @@ export default class PirateParser {
     this.#authFn = authFn;
     this.#errorHandleFn = errorHandleFn;
     this.#streams = streams || 1;
+    this.#headless = headless && true;
+    this.#delay = delay || 0;
 
     console.log('');
     console.log('');
@@ -56,26 +71,41 @@ export default class PirateParser {
   async #startStream() {
     let PAGE = await this.#BROWSER.newPage();
 
-    // Authorization
+    // Авторизация
     if (this.#authFn) {
       PAGE = await this.#authFn(PAGE);
     }
 
+    // Запуск цикла парсинга
     while (this.#stack.length !== 0) {
-      const startTime = Date.now();
+      // Если значение предохранителя больше 15 отключить парсинг
+      if (this.#fuse >= 15) {
+        console.log('PirateParser: КАЖЕТСЯ НАС ЗАБАНИЛИ!');
+        console.log(
+          'Есть только один флаг, и он такой же черный, как наши сердца!'
+        );
+        throw new PirateParserError('You are banned :(((');
+      }
 
+      const startTime = Date.now();
       const item = this.#stack.pop();
-      let dataPart;
+
+      console.log(`PirateParser: парсинг ${item}`);
 
       try {
-        dataPart = await this.#parsingFn(item, PAGE);
+        const dataPart = await this.#parsingFn(item, PAGE);
+        this.#parsedData.push(dataPart);
       } catch (error) {
+        // Увеличить предохранитель на единицу
+        this.#fuse++;
         this.#errorStack.push(item);
         console.log(`PirateParser: ${error.message}`);
       }
 
-      this.#parsedData.push(dataPart);
+      // Обнулить предохранитель, парсинг ссылки прошел успешно
+      this.#fuse = 0;
 
+      // Выдать лог остатка если стек не пуст
       if (this.#stack.length !== 0) {
         console.log(
           `PirateParser: ссылок в очереди: ${
@@ -89,6 +119,11 @@ export default class PirateParser {
           }`
         );
       }
+
+      // Отработать задержку, если она установлена
+      if (this.#delay !== 0) {
+        await PAGE.waitForTimeout(this.#delay);
+      }
     }
 
     await PAGE.close();
@@ -98,7 +133,12 @@ export default class PirateParser {
    * Инициализирует парсер, создает экземпляр браузера
    */
   async init() {
-    this.#BROWSER = await chromium.launch();
+    // Выбор типа запускаемого браузера - с головой или без
+    if (this.#headless) {
+      this.#BROWSER = await firefox.launch();
+    } else {
+      this.#BROWSER = await firefox.launch({ headless: false });
+    }
   }
 
   /**
@@ -121,11 +161,11 @@ export default class PirateParser {
       throw new PirateParserError('Parser is not initialized');
     }
 
-    // Start parsing
+    // Начать парсинг
     console.log('X-X-X-X-X-X-X-X   Поднять паруса!    X-X-X-X-X-X-X-X');
     this.#startTime = Date.now();
 
-    // Start streams
+    // Запустить потоки
     console.log(
       `PirateParser: количество парралельных потоков парсинга: ${this.#streams}`
     );
@@ -135,20 +175,23 @@ export default class PirateParser {
         .map(() => this.#startStream())
     );
 
-    // Parsing is done
+    // Парсинг завершен
     await this.#BROWSER.close();
 
-    // Flat data if there is only one element
-    if (this.#parsedData.length === 1) {
+    // Если был выполнен парсинг одного элемента и это массив - развернуть его
+    if (this.#parsedData.length === 1 && Array.isArray(this.#parsedData[0])) {
       this.#parsedData = this.#parsedData.flat();
     }
 
-    // Run error handler, if #errorStack has records
-    if (this.#errorStack.length !== 0) {
+    // При необходимости, обработать ошибки
+    if (this.#errorHandleFn && this.#errorStack.length !== 0) {
       await this.#errorHandleFn(this.#errorStack);
     }
 
+    // Сохранить данные
     await this.#saveDataFn(this.#parsedData);
+
+    // Бенчмарк
     console.log(
       `PirateParser: средняя скорость парсинга: ${
         Math.round((Date.now() - this.#startTime) / this.#stackLength / 10) /
@@ -165,3 +208,6 @@ class PirateParserError extends Error {
     this.name = 'PirateParserError';
   }
 }
+
+// В конфиге добавить возможность паузы и отключения безголового режима
+// Добавить очередь ошибок отключающую парсинг при превышении количества запросов
